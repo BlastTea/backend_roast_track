600aba055fa20c80afe21693590b6f5d0dd178ff
BlastTea <arya.jbr.999@gmail.com>
2024-04-12 02:53:24 +0700

    fix: set logging from laravel.log into exception.log

--- config/logging.php
+++ config/logging.php
@@ -56,13 +56,13 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/exception/exception.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/exceptions/exception.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
