99d2ce92c2d3fc38cdce2df72ad581ebc65a4ea4
BlastTea <arya.jbr.999@gmail.com>
2024-04-12 00:13:39 +0700

    feat: change logging to daily, change storage_path() to public_path()

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
